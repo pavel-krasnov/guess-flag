b4c4e84a4921ac86071d8b94f18d2c1606ac4856
pavel-krasnov <pavel.krasnov.work@gmail.com>
2020-04-09 16:01:58 +0100

    Updates

---
+++ precache-manifest.7e47260eea6f7f43c03d2538784928cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf7499aec1da4d83430c7430c5a065a2",
+    "url": "/guess-flag/index.html"
+  },
+  {
+    "revision": "72b315652003e26eefdd",
+    "url": "/guess-flag/static/css/main.b5b0a3f2.chunk.css"
+  },
+  {
+    "revision": "0b89cb1050cd65dcda77",
+    "url": "/guess-flag/static/js/2.9145c991.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/guess-flag/static/js/2.9145c991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72b315652003e26eefdd",
+    "url": "/guess-flag/static/js/main.1a4335fd.chunk.js"
+  },
+  {
+    "revision": "38b525c56df0bd9cd2b6",
+    "url": "/guess-flag/static/js/runtime-main.a8532f7d.js"
+  }
+]);
